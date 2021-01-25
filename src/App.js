e938a219f05a91de2fb605463d4dc78b87b24deb
Diogo Pinto <diogopintoes@gmail.com>
2021-01-25 12:06:09 +0000

    Handling Events with Methods

--- src/App.js
+++ src/App.js
@@ -15,15 +15,20 @@ class App extends Component {
     ]
   }
 
+  //It's good pratice to name it 'Handler'. 
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   //'render' is a method of our 'App' class.
   render() {
     return (
       //Remember, react uses JSX and not HTML.
+      //When 'this' is called, it is refering to the 'App'.
       <div className="App">
         <h1>Hi, im a React App.</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
-        {/* 'this' refers to 'App' */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
