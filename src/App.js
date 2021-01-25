1ed10bf02f0d41352b20dcef25c14766df9001ca
Diogo Pinto <diogopintoes@gmail.com>
2021-01-25 11:57:36 +0000

    Understanding and Using State

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
 
+  // The 'state' should be used carefully. It is streated as a JS object.
+  // It should be used carefully, because changes in 'state' cause the DOM to re-render everything.
+  state = {
+    //'persons' is a array of JS objects, with different properties.
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  //'render' is a method of our 'App' class.
+  render() {
     return (
-      // JSX code:
+      //Remember, react uses JSX and not HTML.
       <div className="App">
         <h1>Hi, im a React App.</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button>Switch Name</button>
+        {/* 'this' refers to 'App' */}
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );  
 
-    // React equivalente code:
-    // return React.createElement('div',null, React.createElement('h1', {className: 'App'}, 'Does this work now?'));
   }
 }
 
