f7908a00bf793bb6380806f63456958e9bd38f78
Diogo Pinto <diogopintoes@gmail.com>
2021-01-25 11:37:45 +0000

    Understanding the 'children' Prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
+
     return (
+      // JSX code:
       <div className="App">
-       <h1>Hi, im a React App.</h1>
+        <h1>Hi, im a React App.</h1>
+        <p>This is really working</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
-    );
+    );  
+
+    // React equivalente code:
+    // return React.createElement('div',null, React.createElement('h1', {className: 'App'}, 'Does this work now?'));
   }
 }
 
