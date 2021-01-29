5e059e0c99ee8c8c273ddb047fc0a95c2c3a38dc
Diogo Pinto <diogopintoes@gmail.com>
2021-01-29 17:11:48 +0000

    More on Styled Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import './App.css';
 import Person from './Person/Person';
 //import './Person/Person.css';
 
+const StyledButton = styled.button`
+      background-color: green;
+      color: white;
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: lightgreen;
+        color: black;
+      }
+`;
+
 class App extends Component {
 
   state = {
@@ -98,15 +112,14 @@ class App extends Component {
     }
 
     return (
-        <div className="App">
-          <h1>Hi, im a React App.</h1>
-          {/* Dynamic styling paragraph */}
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
+      <div className="App">
+        <h1>Hi, im a React App.</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
+        {persons}
+      </div>
     );
 
   }
