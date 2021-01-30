94005153cbba86c29df55514da8af4ad058d3a41
Diogo Pinto <diogopintoes@gmail.com>
2021-01-30 10:12:16 +0000

    App Title

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,24 +4,21 @@ import classes from './Cockpit.css';
 const cockpit = (props) => {
 
     let btnClass = '';
-
     if (props.showPersons) {            // Change button color if persons is showing or not.
         btnClass = classes.Red;
     }
 
     const assignedClasses = [];
-
     if (props.persons.length <= 2) {    // Change text color if number of persons is lower than or equals to 2.
         assignedClasses.push(classes.red);
     }
-
-    if (props.persons.length <= 1) {    // Change text weight if number of persons is lower than or equals to1.
+    if (props.persons.length <= 1) {    // Change text weight if number of persons is lower than or equals to 1.
         assignedClasses.push(classes.bold);
     }
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, im a React App.</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
